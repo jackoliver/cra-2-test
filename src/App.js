b198d807716d54a7885abf22cca3533d12060118
Jack Oliver <jeoliver@cloudbees.com>
2023-02-08 14:47:10 +0100

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { MuiTest } from "@jvla/mui-test-pkg";
+import { ContextTest, useContextTest } from "@jvla/context-test-pkg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextTest>
+      <MuiTest>
+        <ConnectedComponent />
+      </MuiTest>
+    </ContextTest>
   );
 }
+
+const ConnectedComponent = () => {
+  const { active } = useContextTest();
+  return (
+    <div>
+      {active.get() ? "active" : "inactive"}
+      <br />
+      <button onClick={() => active.set(!active.get())}>Toggle</button>
+    </div>
+  );
+};
 
 export default App;
